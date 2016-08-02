8ab778a27854e9f591b37b090adfa824052bc4f7
Clifton Roberts <clifton.roberts@me.com>
2016-08-01 23:54:23 -0700

    Improve Parallaxable docs.

--- Sources/Parallaxable.swift
+++ Sources/Parallaxable.swift
@@ -4,12 +4,24 @@
 public protocol Parallaxable: Hashable {
     
     /**
-     Convert `value`, specified over [`from`, `to`], to progress over the unit interval [0, 1].
+     Convert a value over the specified interval to progress over the unit interval.
+     
+     - parameter value: A value over the specified interval, [`from`, `to`].
+     - parameter from:  The start of the interval.
+     - parameter to:    The end of the interval.
+     
+     - returns: Progress over the unit interval, [0, 1].
      */
     static func progress(forValue value: Self, from: Self, to: Self) -> Double
 
     /**
-     Convert `progress`, specified over the unit interval [0, 1], to a value over the interval [`from`, `to`].
+     Convert progress over the unit interval to a value over the specified interval.
+     
+     - parameter progress:  Progress over the unit interval, [0, 1].
+     - parameter from:      The start of the interval.
+     - parameter to:        The end of the interval.
+     
+     - returns: Value over the specified interval, [`from`, `to`].
      */
     static func value(forProgress progress: Double, from: Self, to: Self) -> Self
 }
