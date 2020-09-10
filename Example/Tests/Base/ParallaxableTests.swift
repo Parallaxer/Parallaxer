35c5c08e7f249ed6c238ebde001cfbd59629e256
Clifton <clifton@quizlet.com>
2020-09-10 00:29:10 -0700

    Update tests.

--- Example/Tests/Base/ParallaxableTests.swift
+++ Example/Tests/Base/ParallaxableTests.swift
@@ -7,17 +7,17 @@ final class ParallaxableTests: XCTestCase {
         let from: CGFloat = 8
         let to: CGFloat = 16
         
-        XCTAssertEqual(CGFloat.unitPosition(forValue: 4, from: from, to: to), -0.5)
-        XCTAssertEqual(CGFloat.unitPosition(forValue: 8, from: from, to: to), 0)
-        XCTAssertEqual(CGFloat.unitPosition(forValue: 12, from: from, to: to), 0.5)
-        XCTAssertEqual(CGFloat.unitPosition(forValue: 16, from: from, to: to), 1)
-        XCTAssertEqual(CGFloat.unitPosition(forValue: 20, from: from, to: to), 1.5)
+        XCTAssertEqual(CGFloat.position(forValue: 4, from: from, to: to), -0.5)
+        XCTAssertEqual(CGFloat.position(forValue: 8, from: from, to: to), 0)
+        XCTAssertEqual(CGFloat.position(forValue: 12, from: from, to: to), 0.5)
+        XCTAssertEqual(CGFloat.position(forValue: 16, from: from, to: to), 1)
+        XCTAssertEqual(CGFloat.position(forValue: 20, from: from, to: to), 1.5)
         
-        XCTAssertEqual(CGFloat.value(atUnitPosition: -0.5, from: from, to: to), 4)
-        XCTAssertEqual(CGFloat.value(atUnitPosition: 0, from: from, to: to), 8)
-        XCTAssertEqual(CGFloat.value(atUnitPosition: 0.5, from: from, to: to), 12)
-        XCTAssertEqual(CGFloat.value(atUnitPosition: 1, from: from, to: to), 16)
-        XCTAssertEqual(CGFloat.value(atUnitPosition: 1.5, from: from, to: to), 20)
+        XCTAssertEqual(CGFloat.value(atPosition: -0.5, from: from, to: to), 4)
+        XCTAssertEqual(CGFloat.value(atPosition: 0, from: from, to: to), 8)
+        XCTAssertEqual(CGFloat.value(atPosition: 0.5, from: from, to: to), 12)
+        XCTAssertEqual(CGFloat.value(atPosition: 1, from: from, to: to), 16)
+        XCTAssertEqual(CGFloat.value(atPosition: 1.5, from: from, to: to), 20)
 
         XCTAssertNil(ParallaxInterval(from: from, to: from))
         XCTAssertNil(ParallaxInterval(from: to, to: to))
@@ -27,17 +27,17 @@ final class ParallaxableTests: XCTestCase {
         let from = CGPoint(x: 8, y: 16)
         let to = CGPoint(x: 16, y: 32)
         
-        XCTAssertEqual(CGPoint.unitPosition(forValue: CGPoint(x: 4, y: 8), from: from, to: to), -0.5)
-        XCTAssertEqual(CGPoint.unitPosition(forValue: CGPoint(x: 8, y: 16), from: from, to: to), 0)
-        XCTAssertEqual(CGPoint.unitPosition(forValue: CGPoint(x: 12, y: 24), from: from, to: to), 0.5)
-        XCTAssertEqual(CGPoint.unitPosition(forValue: CGPoint(x: 16, y: 32), from: from, to: to), 1)
-        XCTAssertEqual(CGPoint.unitPosition(forValue: CGPoint(x: 20, y: 40), from: from, to: to), 1.5)
+        XCTAssertEqual(CGPoint.position(forValue: CGPoint(x: 4, y: 8), from: from, to: to), -0.5)
+        XCTAssertEqual(CGPoint.position(forValue: CGPoint(x: 8, y: 16), from: from, to: to), 0)
+        XCTAssertEqual(CGPoint.position(forValue: CGPoint(x: 12, y: 24), from: from, to: to), 0.5)
+        XCTAssertEqual(CGPoint.position(forValue: CGPoint(x: 16, y: 32), from: from, to: to), 1)
+        XCTAssertEqual(CGPoint.position(forValue: CGPoint(x: 20, y: 40), from: from, to: to), 1.5)
         
-        XCTAssertEqual(CGPoint.value(atUnitPosition: -0.5, from: from, to: to), CGPoint(x: 4, y: 8))
-        XCTAssertEqual(CGPoint.value(atUnitPosition: 0, from: from, to: to), CGPoint(x: 8, y: 16))
-        XCTAssertEqual(CGPoint.value(atUnitPosition: 0.5, from: from, to: to), CGPoint(x: 12, y: 24))
-        XCTAssertEqual(CGPoint.value(atUnitPosition: 1, from: from, to: to), CGPoint(x: 16, y: 32))
-        XCTAssertEqual(CGPoint.value(atUnitPosition: 1.5, from: from, to: to), CGPoint(x: 20, y: 40))
+        XCTAssertEqual(CGPoint.value(atPosition: -0.5, from: from, to: to), CGPoint(x: 4, y: 8))
+        XCTAssertEqual(CGPoint.value(atPosition: 0, from: from, to: to), CGPoint(x: 8, y: 16))
+        XCTAssertEqual(CGPoint.value(atPosition: 0.5, from: from, to: to), CGPoint(x: 12, y: 24))
+        XCTAssertEqual(CGPoint.value(atPosition: 1, from: from, to: to), CGPoint(x: 16, y: 32))
+        XCTAssertEqual(CGPoint.value(atPosition: 1.5, from: from, to: to), CGPoint(x: 20, y: 40))
 
         XCTAssertNil(ParallaxInterval(from: from, to: from))
         XCTAssertNil(ParallaxInterval(from: to, to: to))
@@ -47,17 +47,17 @@ final class ParallaxableTests: XCTestCase {
         let from: Double = 8
         let to: Double = 16
         
-        XCTAssertEqual(Double.unitPosition(forValue: 4, from: from, to: to), -0.5)
-        XCTAssertEqual(Double.unitPosition(forValue: 8, from: from, to: to), 0)
-        XCTAssertEqual(Double.unitPosition(forValue: 12, from: from, to: to), 0.5)
-        XCTAssertEqual(Double.unitPosition(forValue: 16, from: from, to: to), 1)
-        XCTAssertEqual(Double.unitPosition(forValue: 20, from: from, to: to), 1.5)
+        XCTAssertEqual(Double.position(forValue: 4, from: from, to: to), -0.5)
+        XCTAssertEqual(Double.position(forValue: 8, from: from, to: to), 0)
+        XCTAssertEqual(Double.position(forValue: 12, from: from, to: to), 0.5)
+        XCTAssertEqual(Double.position(forValue: 16, from: from, to: to), 1)
+        XCTAssertEqual(Double.position(forValue: 20, from: from, to: to), 1.5)
         
-        XCTAssertEqual(Double.value(atUnitPosition: -0.5, from: from, to: to), 4)
-        XCTAssertEqual(Double.value(atUnitPosition: 0, from: from, to: to), 8)
-        XCTAssertEqual(Double.value(atUnitPosition: 0.5, from: from, to: to), 12)
-        XCTAssertEqual(Double.value(atUnitPosition: 1, from: from, to: to), 16)
-        XCTAssertEqual(Double.value(atUnitPosition: 1.5, from: from, to: to), 20)
+        XCTAssertEqual(Double.value(atPosition: -0.5, from: from, to: to), 4)
+        XCTAssertEqual(Double.value(atPosition: 0, from: from, to: to), 8)
+        XCTAssertEqual(Double.value(atPosition: 0.5, from: from, to: to), 12)
+        XCTAssertEqual(Double.value(atPosition: 1, from: from, to: to), 16)
+        XCTAssertEqual(Double.value(atPosition: 1.5, from: from, to: to), 20)
 
         XCTAssertNil(ParallaxInterval(from: from, to: from))
         XCTAssertNil(ParallaxInterval(from: to, to: to))
@@ -67,17 +67,17 @@ final class ParallaxableTests: XCTestCase {
         let from: Float = 8
         let to: Float = 16
         
-        XCTAssertEqual(Float.unitPosition(forValue: 4, from: from, to: to), -0.5)
-        XCTAssertEqual(Float.unitPosition(forValue: 8, from: from, to: to), 0)
-        XCTAssertEqual(Float.unitPosition(forValue: 12, from: from, to: to), 0.5)
-        XCTAssertEqual(Float.unitPosition(forValue: 16, from: from, to: to), 1)
-        XCTAssertEqual(Float.unitPosition(forValue: 20, from: from, to: to), 1.5)
+        XCTAssertEqual(Float.position(forValue: 4, from: from, to: to), -0.5)
+        XCTAssertEqual(Float.position(forValue: 8, from: from, to: to), 0)
+        XCTAssertEqual(Float.position(forValue: 12, from: from, to: to), 0.5)
+        XCTAssertEqual(Float.position(forValue: 16, from: from, to: to), 1)
+        XCTAssertEqual(Float.position(forValue: 20, from: from, to: to), 1.5)
         
-        XCTAssertEqual(Float.value(atUnitPosition: -0.5, from: from, to: to), 4)
-        XCTAssertEqual(Float.value(atUnitPosition: 0, from: from, to: to), 8)
-        XCTAssertEqual(Float.value(atUnitPosition: 0.5, from: from, to: to), 12)
-        XCTAssertEqual(Float.value(atUnitPosition: 1, from: from, to: to), 16)
-        XCTAssertEqual(Float.value(atUnitPosition: 1.5, from: from, to: to), 20)
+        XCTAssertEqual(Float.value(atPosition: -0.5, from: from, to: to), 4)
+        XCTAssertEqual(Float.value(atPosition: 0, from: from, to: to), 8)
+        XCTAssertEqual(Float.value(atPosition: 0.5, from: from, to: to), 12)
+        XCTAssertEqual(Float.value(atPosition: 1, from: from, to: to), 16)
+        XCTAssertEqual(Float.value(atPosition: 1.5, from: from, to: to), 20)
 
         XCTAssertNil(ParallaxInterval(from: from, to: from))
         XCTAssertNil(ParallaxInterval(from: to, to: to))
