fd21b0016a90a753aeb82cb4a2f23c5dca41182a
Clifton <clifton@quizlet.com>
2021-01-06 16:30:26 -0800

    Size change.

--- Example/Playgrounds/Visualizer.playground/Contents.swift
+++ Example/Playgrounds/Visualizer.playground/Contents.swift
@@ -8,7 +8,7 @@ import UIKit
 
 // MARK: Static visualization example.
 
-let visualizer = ParallaxView(frame: CGRect(x: 0, y: 0, width: 400, height: 600))
+let visualizer = ParallaxView(frame: CGRect(x: 0, y: 0, width: 500, height: 600))
 
 let parallax = Observable.just(7)
     .parallax(over: .interval(from: 0, to: 10), visualizer: visualizer)
@@ -22,7 +22,7 @@ visualizer
 
 // MARK: Interactive visualization example. (Make sure the "live view" is visible in the editor.)
 
-let interactiveView = InteractiveParallaxView(frame: CGRect(x: 0, y: 0, width: 400, height: 600))
+let interactiveView = InteractiveParallaxView(frame: CGRect(x: 0, y: 0, width: 500, height: 600))
 
 let sliderInterval = Observable<ParallaxInterval<Float>>.interval(
     from: interactiveView.slider.minimumValue,
